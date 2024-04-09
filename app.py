9183c903a12f2a1bebfa00e3e60f555d5ab574bc
Jan Lerski <jaler@miracle42.dk>
2024-04-09 10:55:03 +0200

    gitops

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Jan! ArgoCD sure is Cool, but a little patience in the deployment part !!'
+    return 'Hello Jan ! ArgoCD sure is Cool, but a little patience in the deployment part !!'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
